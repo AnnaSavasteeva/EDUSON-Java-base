73ec3729df667b3ebfe840ff9f79ae791d88e5f9
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-16 19:36:58 +0300

    Add code for classwork 4

---
+++ src/ClassWorkForArrays.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/**
+ * @author annasavasteeva
+ * @date 16.03.2026
+ */
+class ClassWorkForArrays {
+
+    void processClassWorkForArrays() {
+        System.out.println("---Classwork---");
+        int randomIntsAmount = 5;
+        int[] randomIntsArray = new int[randomIntsAmount];
+        for (int i = 0; i < randomIntsAmount; i++) {
+            randomIntsArray[i] = (int) (Math.random() * 101);
+        }
+        System.out.printf("Array before squaring: %s%n", Arrays.toString(randomIntsArray));
+        for (int i = 0; i < randomIntsAmount; i++) {
+            int arrayEl = randomIntsArray[i];
+            randomIntsArray[i] = (int) Math.pow(arrayEl, 2);
+        }
+        System.out.printf("Array after squaring: %s%n", Arrays.toString(randomIntsArray));
+        System.out.print("Array in reverse order: ");
+        for (int i = randomIntsArray.length - 1; i >= 0; i--) {
+            System.out.print(randomIntsArray[i] + " ");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
 //        new HomeWork1().processHW1();
-
-        new HomeWork2().processHW2();
+//        new HomeWork2().processHW2();
+        new ClassWorkForArrays().processClassWorkForArrays();
     }
 }
