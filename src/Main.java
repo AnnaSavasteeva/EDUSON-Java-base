b7e4705933c9eb832480a66caf90093cd4ee1d6d
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-20 22:45:09 +0300

    Add task 1 for homework 5

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,46 @@
-import arrays.OneDimensionalArrays;
+import utils.ClassWithUtilsMethods;
+
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        new OneDimensionalArrays().processTasks();
+//        new OneDimensionalArrays().processTasks();
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.printf("""
+                ---Задача 1---
+                1.1 Создать двумерный массив, заполнить его случайными числами.
+                1.2 Добавить к каждому значению число, которое пользователь будет вводить с консоли.
+                1.3 Найти сумму всех получившихся элементов и вывести в консоль.%n""");
+        System.out.print("Введите ширину массива: ");
+        int arrayWidth = sc.nextInt();
+        System.out.print("Введите высоту массива: ");
+        int arrayHeight = sc.nextInt();
+        int[][] randomIntsArray = new int[arrayWidth][arrayHeight];
+        for (int i = 0; i < randomIntsArray.length; i++) {
+            for (int j = 0; j < randomIntsArray[i].length; j++) {
+                randomIntsArray[i][j] = ClassWithUtilsMethods.getRandomInt();
+            }
+        }
+        System.out.printf("Исходный массив: %s%n", Arrays.deepToString(randomIntsArray));
+        System.out.print("Введите число, которое нужно прибавить к каждому элементу массива: ");
+        int userInt = sc.nextInt();
+        for (int i = 0; i < randomIntsArray.length; i++) {
+            for (int j = 0; j < randomIntsArray[i].length; j++) {
+                randomIntsArray[i][j] += userInt;
+            }
+        }
+        System.out.printf("Обновленый массив: %s%n", Arrays.deepToString(randomIntsArray));
+        int arrayIntsSum = 0;
+        for (int[] innerArray : randomIntsArray) {
+            for (int anInt : innerArray) {
+                arrayIntsSum += anInt;
+            }
+        }
+        System.out.printf("Сумма всех элементов массива: %d%n", arrayIntsSum);
     }
 }
---
+++ src/arrays/MultidimensionalArrays.java
@@ -0,0 +1,8 @@
+package arrays;
+
+/**
+ * @author annasavasteeva
+ * @date 19.03.2026
+ */
+public class MultidimensionalArrays {
+}
