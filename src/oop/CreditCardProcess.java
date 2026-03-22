cf6f0f1742d5bcc2bb62dcaf676c3d58a62fb116
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-22 22:17:31 +0300

    Add Scanner for test values

--- src/oop/CreditCardProcess.java
+++ src/oop/CreditCardProcess.java
@@ -1,5 +1,7 @@
 package oop;
 
+import java.util.Scanner;
+
 /**
  * @author annasavasteeva
  * @date 21.03.2026
@@ -7,16 +9,25 @@ package oop;
 public class CreditCardProcess {
 
     public void runCreditCardHomework() {
+        Scanner sc = new Scanner(System.in);
 
-        CreditCard card1 = new CreditCard("407028100000000001111", 100000);
-        CreditCard card2 = new CreditCard("407028100000000002222", 200000);
-        CreditCard card3 = new CreditCard("407028100000000003333", 300000);
+        System.out.print("Укажите сумму, которую необходимо положить на счет 1: ");
+        CreditCard card1 = new CreditCard("407028100000000001111", sc.nextInt());
+        System.out.print("Укажите сумму, которую необходимо положить на счет 2: ");
+        CreditCard card2 = new CreditCard("407028100000000002222", sc.nextInt());
+        System.out.print("Укажите сумму, которую необходимо положить на счет 3: ");
+        CreditCard card3 = new CreditCard("407028100000000003333", sc.nextInt());
         showCreditCardsBalance(card1, card2, card3);
 
-        card1.creditAccount(50000);
-        card2.creditAccount(20000);
-        card3.withdrawMoney(75000);
+        System.out.print("Укажите сумму пополнения счета 1: ");
+        card1.creditAccount(sc.nextInt());
+        System.out.print("Укажите сумму пополнения счета 2: ");
+        card2.creditAccount(sc.nextInt());
+        System.out.print("Укажите сумму списания со счета 3: ");
+        card3.withdrawMoney(sc.nextInt());
         showCreditCardsBalance(card1, card2, card3);
+
+        sc.close();
     }
 
     private void showCreditCardsBalance(CreditCard... cards) {
