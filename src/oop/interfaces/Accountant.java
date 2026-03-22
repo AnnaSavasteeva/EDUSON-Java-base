0167a384b12f2320ffbdf5234a28ba89a04a3ba7
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-22 23:05:57 +0300

    Finish task 1

--- src/oop/interfaces/Accountant.java
+++ src/oop/interfaces/Accountant.java
@@ -4,5 +4,12 @@ package oop.interfaces;
  * @author annasavasteeva
  * @date 22.03.2026
  */
-public class Accountant {
+public class Accountant implements JobPosition {
+
+    private final String jobPositionName = "Бухгалтер";
+
+    @Override
+    public void printJobPositionName() {
+        System.out.printf("Должность '%s'%n", this.jobPositionName);
+    }
 }
--- src/oop/interfaces/Manager.java
+++ src/oop/interfaces/Manager.java
@@ -4,5 +4,12 @@ package oop.interfaces;
  * @author annasavasteeva
  * @date 22.03.2026
  */
-public class Manager {
+public class Manager implements JobPosition {
+
+    private final String jobPositionName = "Директор";
+
+    @Override
+    public void printJobPositionName() {
+        System.out.printf("Должность '%s'%n", this.jobPositionName);
+    }
 }
