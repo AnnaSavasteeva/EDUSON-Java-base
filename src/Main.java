68dbaf9458aee18b2642a5cdc841b2afce7a4f7b
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-23 23:26:13 +0300

    Finish homework 2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import oop.abstract_classes.GeometricFigureProcess;
+
 public class Main {
 
     public static void main(String[] args) {
 
 //        new HomeWork1().processHW1();
 
-        new HomeWork2().processHW2();
+//        new HomeWork2().processHW2();
+
+        new GeometricFigureProcess().runHomework();
     }
 }
