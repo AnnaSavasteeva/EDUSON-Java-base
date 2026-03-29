a33f974e80899da0fab0e41d8b154e2b926ae8a0
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-29 23:28:12 +0300

    Add abstract Doctor

---
+++ src/oop/abstract_classes/Doctor.java
@@ -0,0 +1,20 @@
+package oop.abstract_classes;
+
+/**
+ * @author annasavasteeva
+ * @date 29.03.2026
+ */
+public abstract class Doctor {
+
+    private String doctorName;
+
+    public abstract void treat();
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+}
