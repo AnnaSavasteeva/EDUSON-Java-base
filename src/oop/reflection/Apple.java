08b75ca52c2abb944ece5c37373856c4a1f4ddaf
annasavasteeva <anna.savasteeva@gmail.com>
2026-03-30 00:05:55 +0300

    Create Apple

---
+++ src/oop/reflection/Apple.java
@@ -0,0 +1,22 @@
+package oop.reflection;
+
+/**
+ * @author annasavasteeva
+ * @date 30.03.2026
+ */
+public class Apple {
+
+    private String color;
+
+    public Apple(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
