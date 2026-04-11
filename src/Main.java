01bc6861d58e979d0f5441692505dea549c35404
annasavasteeva <anna.savasteeva@gmail.com>
2026-04-11 13:54:21 +0300

    Create class for hw and move task 1 to it

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,9 @@
-import java.util.*;
+import base.strings.StringsOperations;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        String str1 = "My awesome string first";
-        String str2 = "My awesome string second and it's middle";
-        String str3 = "My awesome string third and it's final";
-
-        String[] userStringsArray = {str1, str2, str3};
-        Arrays.sort(userStringsArray, Comparator.comparing(String::length));
-
-        System.out.printf("Самая короткая строка: `%s`. Ее длина — %d.%n", userStringsArray[0], userStringsArray[0].length());
-        System.out.printf("Самая длинная строка: `%s`. Ее длина — %d.%n", userStringsArray[2], userStringsArray[2].length());
-
-        System.out.println("---------");
-
-        System.out.println("Строки в порядке возрастания значений их длины:");
-        for(String str : userStringsArray) {
-            System.out.println(str);
-        }
-
-        System.out.println("---------");
-        double strAverageLength = Arrays.stream(userStringsArray).mapToInt(String::length).average().orElse(0.0);
-        System.out.printf("Средняя длина строки: %f%n", strAverageLength);
-        for(String str : userStringsArray) {
-            if (str.length() < strAverageLength) {
-                System.out.printf("`%s`. Длина — %d.%n", str, str.length());
-            }
-        }
-
-        System.out.println("---------");
-        String taskResult = null;
-        for(String str : userStringsArray) {
-            String[] words = str.split("\\s+");
-            for (String word : words) {
-                if (isWordWithUniqueLetters(word)) {
-                    taskResult = word;
-                    break;
-                }
-            }
-        }
-        if (taskResult == null) {taskResult = "слово не найдено";}
-        System.out.printf("Слово, состоящее из различных символов: %s%n", taskResult);
-    }
-
-    private static boolean isWordWithUniqueLetters(String word) {
-        Set<Character> letters = new HashSet<>();
-        for (char letter : word.toCharArray()) {
-            if (letters.contains(letter)) {
-                return false;
-            }
-            letters.add(letter);
-        }
-        return true;
+        new StringsOperations().runHomework();
     }
 }
---
+++ src/base/strings/StringsOperations.java
@@ -0,0 +1,47 @@
+package base.strings;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * @author annasavasteeva
+ * @date 11.04.2026
+ */
+public class StringsOperations {
+
+//    private final Scanner sc;
+    private final String str1;
+    private final String str2;
+    private final String str3;
+    private final String[] userStringsArray;
+
+    public StringsOperations() {
+//        System.out.println("Введите 3 строки:");
+//        this.sc = new Scanner(System.in);
+        str1 = "My awesome string first";
+        str2 = "My awesome string second and it's middle";
+        str3 = "My awesome string third and it's final";
+        userStringsArray = new String[]{str1, str2, str3};
+    }
+
+    public void runHomework() {
+        System.out.println("------------------");
+        System.out.printf("Ваши строки:%n %s%n", Arrays.deepToString(userStringsArray));
+        processTaskOne();
+        System.out.println("------------------");
+
+        System.out.println("------------------");
+//        sc.close();
+    }
+
+    private void processTaskOne() {
+        System.out.printf("""
+                -----Задача 1-----
+                Найти самую короткую и самую длинную строки.
+                Вывести найденные строки и их длину.%n---%n""");
+        Arrays.sort(userStringsArray, Comparator.comparing(String::length));
+        System.out.printf("Самая короткая строка: `%s`. Ее длина — %d.%n", userStringsArray[0], userStringsArray[0].length());
+        System.out.printf("Самая длинная строка: `%s`. Ее длина — %d.%n", userStringsArray[2], userStringsArray[2].length());
+    }
+}
