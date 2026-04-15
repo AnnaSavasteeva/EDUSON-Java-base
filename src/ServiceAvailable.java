c564efb808db9d4a37763454ddbc9984b9859ccd
annasavasteeva <anna.savasteeva@gmail.com>
2026-04-16 00:12:59 +0300

    Add two objects types

---
+++ src/ServiceAvailable.java
@@ -0,0 +1,11 @@
+/**
+ * @author annasavasteeva
+ * @date 16.04.2026
+ */
+public class ServiceAvailable {
+
+    private String code;
+    private String name;
+    private Long cost;
+    private String term;
+}
---
+++ src/ServiceConnected.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+
+/**
+ * @author annasavasteeva
+ * @date 16.04.2026
+ */
+public class ServiceConnected {
+
+    private String code;
+    private String name;
+    private Long cost;
+    private LocalDateTime connectionDate;
+    private LocalDateTime endDate;
+}
