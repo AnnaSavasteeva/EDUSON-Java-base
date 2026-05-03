a97853fa555cf677c7b6bdfc2d66beb58bbc1900
annasavasteeva <anna.savasteeva@gmail.com>
2026-05-03 21:21:00 +0300

    Add comments with steps for app and update Main

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+import files_input_output.DocNumbersApp;
 import files_input_output.RomeoAndJulietProcess;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        new RomeoAndJulietProcess().runApp();
+//        new RomeoAndJulietProcess().runApp();
+        new DocNumbersApp().runApp();
     }
 }
