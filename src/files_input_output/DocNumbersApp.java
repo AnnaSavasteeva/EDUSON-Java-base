eadecbbd042341e1f12f9684766eeb401d8c187f
annasavasteeva <anna.savasteeva@gmail.com>
2026-05-06 00:11:09 +0300

    Create file with numbers and finish app logic

--- src/files_input_output/DocNumbersApp.java
+++ src/files_input_output/DocNumbersApp.java
@@ -4,6 +4,7 @@ import files_input_output.exceptions.EmptyFileException;
 
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,25 +20,72 @@ import static java.lang.String.format;
  */
 public class DocNumbersApp {
 
+    private static final String REPORTS_DIR = "resources/files";
+    private static final String DOC_NUMBERS_REPORT = "valid_doc_nums.txt";
+    private static final String INVALID_DOC_NUMBERS_REPORT = "invalid_doc_nums.txt";
+
     public void runApp() {
         try (Scanner sc = new Scanner(System.in)) {
-            File file = new File(getPathToFile(sc));
-            List<Character> docNumbersAsChars = extractCharsFromFile(file);
+            File fileToRead = new File(getPathToFile(sc));
+            List<Character> docNumbersAsChars = extractCharsFromFile(fileToRead);
 
             if (docNumbersAsChars.isEmpty()) throw new EmptyFileException();
+
+            File directory = new File(REPORTS_DIR);
+            if (!directory.exists()) directory.mkdirs();
+            File valid_numbers = createFile(directory, DOC_NUMBERS_REPORT);
+            File invalid_numbers = createFile(directory, INVALID_DOC_NUMBERS_REPORT);
 
             String docNumbersString = docNumbersAsChars.stream().map(String::valueOf).collect(Collectors.joining());
             List<String> docNumbersList = Arrays.asList(docNumbersString.split("\n"));
             docNumbersList.forEach(docNumber -> {
-//        TODO: создать метод валидации номера документа:
-//        - начинается с docnum или contract
-//        - содержит только буквы и цифры
-//        - длина == 15 символов
+//        TODO: если номер невалиден, добавить информацию, почему он невалиден
+                StringBuilder numberProblems = new StringBuilder();
+                if (!isStartValid(docNumber)) numberProblems.append("- номер должен начинаться с 'docnum' или 'contract'");
+                if (!isLengthValid(docNumber)) numberProblems.append("- номер должен состоять из 15 символов");
+                if (!isContentValid(docNumber)) numberProblems.append("- номер должен состоять из только из букв и цифр");
 
-//        TODO: если номер валиден, записать в соответствующий файл-отчет
-//        TODO: если номер невалиден, записать в другой файл-отчет и добавить информацию, почему он невалиден
+                if (!numberProblems.isEmpty()) {
+                    String entry = format("[%s]:", docNumber);
+                    numberProblems.insert(0, entry);
+                    writeStringToFile(invalid_numbers, numberProblems.toString());
+                } else {
+                    writeStringToFile(valid_numbers, docNumber);
+                }
             });
         }
+    }
+
+    private void writeStringToFile(File file, String str) {
+        try (FileWriter fw = new FileWriter(file.getPath())) {
+            fw.write(str);
+            fw.flush();
+        } catch (IOException e) {
+            e.printStackTrace() ;
+        }
+    }
+
+    private boolean isContentValid(String docNumber) {
+        return docNumber.matches("^[a-zA-Z0-9]+$");
+    }
+
+    private boolean isLengthValid(String docNumber) {
+        return docNumber.length() == 15;
+    }
+
+    private boolean isStartValid(String docNumber) {
+        return docNumber.startsWith("docnum") || docNumber.startsWith("contract");
+    }
+
+    private File createFile(File directory, String fileName) {
+        File file = new File(directory, fileName);
+        try {
+            if (file.createNewFile()) System.out.println("Создан файл: " + file.getAbsoluteFile());
+            else System.out.println("Файл уже существует");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return file;
     }
 
     private List<Character> extractCharsFromFile (File file) {
@@ -48,7 +96,7 @@ public class DocNumbersApp {
                 charsList.add((char) i);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
         return charsList;
     }
