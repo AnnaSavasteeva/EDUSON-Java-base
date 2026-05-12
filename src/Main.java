8f6c5ca23ebbadab719c7697ba154e106db476f0
annasavasteeva <anna.savasteeva@gmail.com>
2026-05-12 23:33:24 +0300

    Add task 1 for homework 18 and readme

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
-import threads.ClassworkPracticeSynchronization;
+import streams.StreamApp;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+import static utils.ClassWithUtilsMethods.getRandomIntList;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        new ClassworkPracticeSynchronization().runApp();
+        new StreamApp().runApp();
     }
 }
---
+++ src/streams/StreamApp.java
@@ -0,0 +1,46 @@
+package streams;
+
+import java.util.List;
+import java.util.Scanner;
+
+import static utils.ClassWithUtilsMethods.getRandomIntList;
+
+/**
+ * @author annasavasteeva
+ * @date 12.05.2026
+ */
+public class StreamApp {
+
+    public void runApp() {
+        System.out.println("----------");
+        System.out.println("ЗАДАЧА 1: список чисел");
+        runIntegersListProcessing();
+        System.out.println("----------");
+        System.out.println("----------");
+        System.out.println("ЗАДАЧА 2: словарь");
+        runMapProcessing();
+        System.out.println("----------");
+    }
+
+    private void runMapProcessing() {}
+
+    private void runIntegersListProcessing() {
+        System.out.print("Сколько чисел будет в списке? ");
+        int listSize = new Scanner(System.in).nextInt();
+        List<Integer> intsList = getRandomIntList(listSize);
+        System.out.printf("Исходный список: %s%n", intsList);
+        for (int i = 0; i < listSize - 2; i+=2) {
+            intsList.add(intsList.get(i));
+        }
+        System.out.printf("Список после дублирования элементов: %s%n", intsList);
+
+        List<Integer> uniqueList = intsList.stream().distinct().toList();
+        System.out.printf("Список без дубликатов: %s%n", uniqueList);
+
+        List<Integer> evenIntsList = uniqueList.stream().filter(number -> number % 2 == 0).toList();
+        System.out.printf("Список с четными числами: %s%n", evenIntsList);
+
+        int intsSum = evenIntsList.stream().mapToInt(Integer::intValue).sum();
+        System.out.printf("Сумма оставшихся в списке элементов: %d%n", intsSum);
+    }
+}
